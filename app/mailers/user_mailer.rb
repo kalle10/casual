d5cc93c747f3955c944b8890868f66edde1f405c
kalle <malmiharju@btinternet.com>
2014-03-20 15:49:48 +0000

    plah

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def contact_email(name, email, phone, message)
   	@name, @email, @phone, @message = name, email, phone, message
-    mail(to: "your@friend.com", subject: "#{name} wants to contact us!")
+    mail(to: "malmiharju@btinternet.com", subject: "#{name} wants to contact us!")
   end
 end
