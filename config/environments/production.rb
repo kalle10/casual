494df151cd7e7c57a1449da418baec2184d838b7
kalle <malmiharju@btinternet.com>
2014-03-21 15:52:48 +0000

    changed mail to use mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,10 @@ Casual::Application.configure do
   # config.action_mailer.default_charset = "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'k.alfr3d@gmail.com',
-    password:             'pasanen1',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'makela.heroku.com',
+    :authentication => :plain  }
 end
