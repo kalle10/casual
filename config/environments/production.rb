790668003e2b99bb36ef04a3493ddaff7ec95527
kalle <malmiharju@btinternet.com>
2014-03-21 15:32:05 +0000

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Casual::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -76,5 +76,15 @@ Casual::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  # config.log_formatter = ::Logger::Formatter.new
+  # config.action_mailer.default_charset = "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'k.alfr3d@gmail.com',
+    password:             'pasanen1',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
